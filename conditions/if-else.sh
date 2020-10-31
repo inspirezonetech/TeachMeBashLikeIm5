c4ba852596de5243128e89303212412c30b5a7b8
Sarah Abderemane <sarahabderemane@gmail.com>
2020-10-31 17:00:31 +0100

    Add if-else tutorial and challenge

--- conditions/if-else.sh
+++ conditions/if-else.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: How using if-else work
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# You can write condition same as in real life
+# For example, if the person in front of me is Sarah, I will tell you "Hi Sarah !" 
+# else, I will tell you "Hi anonymous !"
+
+# In bash, it will be : if my variable person equal Sarah, I will tell you "Hi Sarah !" 
+# else, I will tell you "Hi anonymous !"
+
+
+person="Sarah"
+
+# brackets are needed for the condition
+if [ $person = "Bob" ]
+then
+  echo "Hi Sarah !"
+else
+  echo "Hi anonymous !"
+fi
+
+# if you want to say something if the condition is true but say nothing it's not, you
+# don't put the else part, just like this : 
+if [ $person = "Sarah" ]
+then
+  echo "Hi Sarah !"
+fi
 
 # ------------------------------------------------------------------------------------
 # Challenge: list challenges to be completed here. minimum of one challenge per tutorial
 # ------------------------------------------------------------------------------------
+
+# Write a condition to say Hi to yourself and if it's not your first name 
+# it will say "No, try again"
