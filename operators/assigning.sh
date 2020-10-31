587a07b69a0e728fddf410407e1f00df242938bb
Sarah Abderemane <sarahabderemane@gmail.com>
2020-10-31 18:41:59 +0100

    add assigning operators tutorial and challenge

--- operators/assigning.sh
+++ operators/assigning.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: assigning operators
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# Assignment is a bit same as a box where you store something
+# the "box" is called a variable, and to store something, you put what you want
+# at the right of an equal and at the left, the name of the variable
+
+# Example
+fruit="banana"
+
+# be careful, no spaces
+
+# we can see the result here
+echo "The value of \"fruit\" is $fruit."
+
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: assigning your favorite celebrity
 # ------------------------------------------------------------------------------------
+
+# Choose the name of your favorite celebrity, assign it to the variable "name"
+# and show the value of name with an echo just like above
