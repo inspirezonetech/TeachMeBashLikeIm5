435986ed8a037b79f0884d0efac94273ae8c9b84
Sarah Abderemane <sarahabderemane@gmail.com>
2020-11-03 08:51:24 +0100

    add assigning operators tutorial and challenge (#13)
    * add assigning operators tutorial and challenge
    
    * improve tutorial
    
    * fix lint

--- operators/assigning.sh
+++ operators/assigning.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: assigning operators
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# Assignment is a bit same as a box where you store something
+# the "box" is called a variable, and to store something, you put what you want
+# at the right of an equal and at the left, the name of the variable
+
+# Example
+fruit="banana"
+
+# be careful, no spaces
+
+# we use the "$" keyword to see the value of a variable, just like this :
+echo "The value of \"fruit\" is $fruit."
+
+# you have also with double brackets for assigning, especially for arithmetic operations
+(( a=10+5 ))
+echo "The value of \"a\" is now $a."
+
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: assigning your favorite celebrity
 # ------------------------------------------------------------------------------------
+
+# Choose the name of your favorite celebrity, assign it to the variable "name"
+# and show the value of name with an echo just like above
