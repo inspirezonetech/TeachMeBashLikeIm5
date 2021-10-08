fd7cbc352a138861e452f3832e7a407342459ec3
Talas <74327184+IAmTalas@users.noreply.github.com>
2021-10-08 20:16:46 +0330

    Add #17
    Add executing-bash-files.sh

--- getting-started/executing-bash-files.sh
+++ getting-started/executing-bash-files.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: Running a bash script
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# When our script gets bigger and bigger, we store it in a file.
+# Create a new text file with a .sh extension, then put your commands in it.
+
+vim my-script.sh
+
+# And in order to run a bash script, you should give the file "Execute" permission.
+# If we don't give the "Execute" permission, we may get a permission denied error.
+
+chmod u+x ./my-script.sh
+
+# Now in order to run the file.
+
+./my-script.sh
+# or
+bash my-script.sh
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: write a bash script that, when executed 
+# print a Hello message with your name and show the calendar
 # ------------------------------------------------------------------------------------
