2940042e934864e1894d366805a2139e9e97e31d
Adrian Pop <adi.pop.bv@gmail.com>
2021-10-12 08:55:47 +0300

    Done shellcheck suggestions

--- variables/handling-variables.sh
+++ variables/handling-variables.sh
@@ -10,27 +10,27 @@ variable="hello!"
 # The part that is written left of the '=' operator is called the variable name
 #(or identifier).
 # To access the value of a variable, you need to use the '$' operator like this:
-echo $variable
+echo "$variable"
 
 # When using a variable, it might not have a value, therefore it would be awesome 
 #to be able to have a default value in case that happens. Happily, you can have
 #default values and the syntax is like this:
-echo ${newVariable:-"variable does not have a value"}
+echo "${newVariable:-"variable does not have a value"}"
 
 # To test if a variable has a value and do something based on that information you
 #can use '?' or ':?' as follows:
-echo ${newVariable:?variable has no value} #this will print the text that follows
-										   #':?' as an error
+echo "${newVariable:?variable has no value}" #this will print the text that follows
+											 #':?' as an error
 
 # To assign a value to a variable only if it does not already have one when being
 #querried:
-echo ${newVariable:="have this value from now on"}
+echo "${newVariable:="have this value from now on"}"
 
 # To clear a variable's value you need to do:
 newVariable=
 
 # You can find the length of a string value like this:
-echo ${#variable}
+echo "${#variable}"
 
 # ------------------------------------------------------------------------------------
 # Challenge: How long are you?
