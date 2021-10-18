b227f84a34fc4b69e6b184adddc9b346150c7702
Fum <66768334+funbeedev@users.noreply.github.com>
2021-10-18 20:09:51 +0100

    Update commenting.sh

--- getting-started/commenting.sh
+++ getting-started/commenting.sh
@@ -3,20 +3,20 @@
 # Tutorial: commenting
 # ------------------------------------------------------------------------------------
 
-# Comments are a good way of writing useful information or disable temporarily 
-#some instructions.
+# Comments are a good way of writing useful information in your code or to temporarily disable  
+# some instructions that will not be executed.
 
 # To comment you must use the '#' character. Any text on a line, that follows the
-#'#' character will be considered a comment.
+# '#' character will be considered a comment.
 
 # this is a single-line comment
 echo "hello!" # this is also a single-line comment that follows an instruction
 
 # However, if you would write '!' immediately after a '#', you would create a shebang,
-#an instruction that tells the operating system what interpreter should be used to run
-#the file.
+# an instruction that tells the operating system what interpreter should be used to run
+# the file.
 # As an example take the first line of this file that makes sure the OS will run this
-#using Bash.
+# using Bash.
 
 # ------------------------------------------------------------------------------------
 # Challenge: write your name in a comment!
