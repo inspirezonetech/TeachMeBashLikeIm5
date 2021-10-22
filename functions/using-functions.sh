38454f09ab504b05d3c46e0a377dd5157593597d
Adrian Pop <adi.pop.bv@gmail.com>
2021-10-22 11:52:02 +0300

    Added using-functions tutorial and challenge

--- functions/using-functions.sh
+++ functions/using-functions.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: using functions
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# As it goes with most programming and scripting languages, a function is a small
+# chunk of instructions within your code that can be called all at a time and looks
+# like this:
+function_name1 () {
+	# commands
+	return 0
+}
+# or like this:
+function function_name2 {
+	# commands
+	return 0
+}
+
+# If you want your function to do something based on some data, you should use arguments:
+function function_name3 {
+	echo "$1"
+	return 0
+}
+function_name3 "Hello world!"
+# An argument is a value you give a function to do something with. For example take
+# 'function_name3' where we give it "Hello world!" so it can print it in the terminal.
+# You can give any number of arguments and they can be accessed by using '$' and the
+# position of that argument in the function call.
+
+# Watch out!
+# Any function must be defined before any calls to it are made.
+# If you declare a variable inside a function, it will only be accessible inside that function.
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: create a function that prints something in the terminal and call it
 # ------------------------------------------------------------------------------------
