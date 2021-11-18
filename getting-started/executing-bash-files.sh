ee6fb37894737418800d52e8a10573ac124e9c44
Talas <74327184+IAmTalas@users.noreply.github.com>
2021-11-18 11:03:49 +0330

    Add executing-bash-files.sh (#22)
    * Add #17
    
    Add executing-bash-files.sh
    
    * Update executing-bash-files.sh
    
    * Update executing-bash-files.sh
    
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

--- getting-started/executing-bash-files.sh
+++ getting-started/executing-bash-files.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: How to execute a bash script
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# This will create a file called "my-scipt.sh" containing the command "echo execute my-script.sh".
+echo "echo execute my-script.sh" > my-script.sh
+
+# To run a bash script, you need to give the file "Execute" permission.
+# If we don't give the "Execute" permission, we may get a permission denied error.
+# The following command will give "Execute" permissions to the file just created
+chmod +x ./my-script.sh
+
+# Bash files are executed using the following syntax:
+./my-script.sh
+# or
+bash my-script.sh
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: Write a bash script that, when executed 
+# will print a "Hello" message with your name and the current date
+# Hint: Use echo "$(date)" to print the date
 # ------------------------------------------------------------------------------------
